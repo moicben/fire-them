53138e89e534a089d4e112639da192e9bf06399b
moicben <benjamain.georges@gmail.com>
2025-01-02 00:21:24 +0800

    heroku deploy 6

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,22 @@
-import '@styles/globals.css'
+import '@styles/globals.css';
 import '../styles/landing.css';
 import '../styles/header.css';
 import '../styles/footer.css';
 import '../styles/category.css';
 import '../styles/index.css';
-import '../styles/hero.css'
+import '../styles/hero.css';
 import '../styles/category.css';
 import Head from 'next/head';
 
 function Application({ Component, pageProps }) {
-  <Head>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
-  </Head>
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default Application
+export default Application;
