57ff1d3eef7e6d51f980461f18c468ee8be0e9da
moicben <benjamain.georges@gmail.com>
2025-01-01 23:58:51 +0800

    heroku deploy 6

--- components/Head.js
+++ components/Head.js
@@ -7,6 +7,7 @@ const Head = ({ title, description }) => {
             <meta name="description" content={description} />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link rel="icon" href="/favicon.ico" />
+            <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
         </Head>
     );
 };
