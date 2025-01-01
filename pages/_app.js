60d47e74f7237f97ee0cebb1df9bafef4e94f3df
moicben <benjamain.georges@gmail.com>
2025-01-02 00:19:29 +0800

    heroku deploy 6

--- pages/_app.js
+++ pages/_app.js
@@ -6,8 +6,12 @@ import '../styles/category.css';
 import '../styles/index.css';
 import '../styles/hero.css'
 import '../styles/category.css';
+import Head from 'next/head';
 
 function Application({ Component, pageProps }) {
+  <Head>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
+  </Head>
   return <Component {...pageProps} />
 }
 
