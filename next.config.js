a36ec2490a18a32f43d5a5439386f34f63fbf66c
moicben <benjamain.georges@gmail.com>
2025-01-01 23:17:15 +0800

    heroku deploy 1

--- next.config.js
+++ next.config.js
@@ -1,24 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export', // Utiliser l'exportation statique
-  webpack: (config) => {
-    config.plugins.push({
-      apply: (compiler) => {
-        compiler.hooks.emit.tapAsync('ReplaceDoubleSlashesPlugin', (compilation, callback) => {
-          Object.keys(compilation.assets).forEach((asset) => {
-            if (asset.includes('//')) {
-              const newAsset = asset.replace(/\/\//g, '/');
-              compilation.assets[newAsset] = compilation.assets[asset];
-              delete compilation.assets[asset];
-            }
-          });
-          callback();
-        });
-      },
-    });
-    return config;
-  },
 };
 
 module.exports = nextConfig;
