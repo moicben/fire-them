cccf1bf32abdc42023682f47f2b8063360f8cf72
moicben <benjamain.georges@gmail.com>
2025-01-01 17:22:40 +0800

    build bugs.4

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export', // Utiliser l'exportation statique
+  async redirects() {
+    return [
+      {
+        source: '/lead-backend-developer',
+        destination: '/', // Update this to the correct destination
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
